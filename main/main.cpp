a15a905f89cf230e52d6d21ecab66467f6e6a194
amirkhanyan11 <138108895+amirkhanyan11@users.noreply.github.com>
2024-02-17 14:07:12 +0400

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -5,17 +5,5 @@ using namespace earthtwo;
 
 int main()
 {
-	// earthtwo::Country gelenjik;
-	Country a{"qyalnoc"};
-
-	Country b = a;
-
-	cout << b.get_name() << endl;
-	// ofstream ofs("/Users/aamirkha/Desktop/earthtwo/report/dayCycleReport");
-
-	// f_Daycycle cycle{ofs};
-
-	// cycle(15);
-
 	return 0;
 }
